7f0ae49edb9ed56ac439aa5080c3d725e76d3e85
Lucas N Martins <lucasmartins@railsnapraia.com>
2013-12-15 17:35:59 -0200

    moving thins around

--- lib/mailcannon/adapters/sendgrid.rb
+++ lib/mailcannon/adapters/sendgrid.rb
@@ -4,18 +4,7 @@ module MailCannon::Adapter
     module InstanceMethods
       def send!
         raise 'Invalid Document!' unless self.valid?
-        client = SendGridWebApi::Client.new(ENV['SENDGRID_USERNAME'], ENV['SENDGRID_PASSWORD'])
-        response = client.mail.send(
-          :to => self.to,
-          :toname => self.to_name,
-          :subject => self.subject,
-          :text => self.mail.text,
-          :html => self.mail.html,
-          :from => self.from,
-          :fromname => self.from_name,
-          :bcc => self.bcc,
-          :replyto => self.reply_to
-        )
+        response = send_single_email
         self.after_sent(successfully_sent?(response))
         return response
       end
@@ -26,6 +15,24 @@ module MailCannon::Adapter
     end
     
     private
+    def api_client
+      client = SendGridWebApi::Client.new(ENV['SENDGRID_USERNAME'], ENV['SENDGRID_PASSWORD'])
+    end
+    
+    def send_single_email
+      api_client.mail.send(
+                :to => self.to,
+                :toname => self.to_name,
+                :subject => self.subject,
+                :text => self.mail.text,
+                :html => self.mail.html,
+                :from => self.from,
+                :fromname => self.from_name,
+                :bcc => self.bcc,
+                :replyto => self.reply_to
+              )
+    end
+    
     def successfully_sent?(response)
       if response['message']=='success'
         true
