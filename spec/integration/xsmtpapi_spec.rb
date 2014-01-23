a49b4b865bbe3c6005235e54f32a0bb41503778d
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-01-23 11:09:25 -0200

    fixed expectation

--- spec/integration/xsmtpapi_spec.rb
+++ spec/integration/xsmtpapi_spec.rb
@@ -6,6 +6,7 @@ describe 'X-SMTPAPI compatibility' do
     #   http://sendgrid.com/docs/API_Reference/SMTP_API/unique_arguments.html
     context "generates expected xsmtpapi for #post!" do
       let(:envelope) { build(:envelope_multi, xsmtpapi: { "sub" => { "-email-id-" => ["314159","271828"] }, "unique_args" => { "email_id" => "-email-id-"} }) }
+      let(:expectated_hash) { {"sub"=>{"-email-id-"=>["314159", "271828"], "*|NAME|*"=>["Mail Cannon", "Lucas Martins", "Contact"]}, "unique_args"=>{"email_id"=>"-email-id-"}, "to"=>["mailcannon@railsnapraia.com", "lucasmartins@railsnapraia.com", "contact@railsonthebeach.com"]} }
 
       it "returns true" do
         VCR.use_cassette('mailcannon_adapter_sendgrid_send_bulk') do
@@ -15,12 +16,7 @@ describe 'X-SMTPAPI compatibility' do
         end
         envelope.save
         envelope.reload # content is changed inside the Adapter module
-        expect(envelope.xsmtpapi).to eq({
-          "sub"=>{"*|NAME|*"=>["Mail Cannon", "Lucas Martins", "Contact"]},
-          "unique_args"=>{"email_id"=>"-email-id-"},
-          "to"=>["mailcannon@railsnapraia.com",
-           "lucasmartins@railsnapraia.com",
-           "contact@railsonthebeach.com"]})
+        expect(envelope.xsmtpapi).to eq(expectated_hash)
       end
     end
   end
