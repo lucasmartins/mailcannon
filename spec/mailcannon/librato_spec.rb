772d9e31a1662d0d6c3e83262e1378e8c30669bb
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-02-10 08:23:13 -0200

    forgot to commit librato spec

--- spec/mailcannon/librato_spec.rb
+++ spec/mailcannon/librato_spec.rb
@@ -5,14 +5,14 @@ describe MailCannon::Librato do
     context 'when Librato info is NOT in the ENV' do
       it "returns false" do
         ENV['LIBRATO_USER']=nil
-        ENV['LIBRATO_PASSWORD']=''
+        ENV['LIBRATO_TOKEN']=''
         expect(MailCannon::Librato.available?).to be_false
       end
     end
     context 'when Librato info IS in the ENV' do
       it "returns false" do
         ENV['LIBRATO_USER']='tha.user'
-        ENV['LIBRATO_PASSWORD']='tha.password'
+        ENV['LIBRATO_TOKEN']='tha.password'
         expect(MailCannon::Librato.available?).to be_true
       end
     end
