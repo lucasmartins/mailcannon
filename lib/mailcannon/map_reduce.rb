bbff41b4af08f0177d5580e04305ae8c9a3b35b0
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-03-10 08:40:24 -0300

    adding the "third"

--- lib/mailcannon/map_reduce.rb
+++ lib/mailcannon/map_reduce.rb
@@ -2,11 +2,12 @@ class MailCannon::MapReduce
   # gets the events from the generic event collection into the right envelope 
   def self.grab_events_for_envelope(id)
     #TODO em lotes
-    events = MailCannon::SendgridEvent.where(envelope_id: id).find_and_modify({}, remove: true)
+    events = MailCannon::SendgridEvent.where(envelope_id: id)
     envelope = MailCannon::Envelope.find(id)
     events.each do |e|
       envelope.sendgrid_events << e
     end
+    events
   end
 end
 
