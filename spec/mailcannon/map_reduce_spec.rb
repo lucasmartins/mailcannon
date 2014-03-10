a8b7527ce70c99888d273f02257de542f4f958b6
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-03-10 08:42:31 -0300

    little step

--- spec/mailcannon/map_reduce_spec.rb
+++ spec/mailcannon/map_reduce_spec.rb
@@ -7,11 +7,11 @@ describe MailCannon::MapReduce do
     envelope_a.save
     envelope_b.save
     test_hash = [
-      {envelope_id: envelope_a.id ,email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
-      {envelope_id: envelope_a.id ,email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'open'},
-      {envelope_id: envelope_a.id ,email: 'foo3@bar.com', timestamp: 1322000094, unique_arg: 'my unique arg', event: 'bounce'},
-      {envelope_id: envelope_b.id ,email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
-      {envelope_id: envelope_b.id ,email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'click'}
+      {envelope_id: envelope_a.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
+      {envelope_id: envelope_a.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'open'},
+      {envelope_id: envelope_a.id, email: 'foo3@bar.com', timestamp: 1322000094, unique_arg: 'my unique arg', event: 'bounce'},
+      {envelope_id: envelope_b.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
+      {envelope_id: envelope_b.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'click'}
     ]
     MailCannon::SendgridEvent.insert_bulk(test_hash)
   end
