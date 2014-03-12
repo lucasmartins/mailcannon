710d294dbba3f880b4c9102b4c0fe5b347dea8b4
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-03-12 12:09:00 -0300

    reducing envelope

--- lib/mailcannon/map_reduce.rb
+++ lib/mailcannon/map_reduce.rb
@@ -6,4 +6,125 @@ class MailCannon::MapReduce
     envelope = MailCannon::Envelope.find(id)
     envelope.sendgrid_events.concat(events) unless events.empty?
   end
+
+  def self.statistics_for_envelope(id)
+    MailCannon::SendgridEvent.where(envelope_id: id).map_reduce(MailCannon::MapReduce.map, MailCannon::MapReduce.reduce).out(inline: true)
+  end
+
+  #private
+  def self.map
+    %Q{
+      function () {
+        emit(this.envelope_id, { lead_id: this.lead_id, event: this.event });
+      }
+    }
+  end
+
+  def self.reduce
+    %Q{
+      function (key, values) {
+      var result = {
+        'envelope_statistics': {
+          'envelope_id': key,
+          'events': {
+            'posted': {
+              'count': 0,
+              'leads': []
+            },
+            'processed': {
+              'count': 0,
+              'leads': []
+            },
+            'delivered': {
+              'count': 0,
+              'leads': []
+            },
+            'open': {
+              'count': 0,
+              'leads': []
+            },
+            'click': {
+              'count': 0,
+              'leads': []
+            },
+            'deferred': {
+              'count': 0,
+              'leads': []
+            },
+            'spam_report': {
+              'count': 0,
+              'leads': []
+            },
+            'spam': {
+              'count': 0,
+              'leads': []
+            },
+            'unsubscribe': {
+              'count': 0,
+              'leads': []
+            },
+            'drop': {
+              'count': 0,
+              'leads': []
+            },
+            'bounce': {
+              'count': 0,
+              'leads': []
+            }
+          }
+        }
+      };
+
+      values.forEach(function(value) {
+        switch (value['event']) {
+          case 'posted':
+            result['envelope_statistics']['events']['posted']['count']++;
+            result['envelope_statistics']['events']['posted']['leads'].push(value['lead_id']);
+          break;
+          case 'processed':
+            result['envelope_statistics']['events']['processed']['count']++;
+            result['envelope_statistics']['events']['processed']['leads'].push(value['lead_id']);
+          break;
+          case 'delivered':
+            result['envelope_statistics']['events']['delivered']['count']++;
+            result['envelope_statistics']['events']['delivered']['leads'].push(value['lead_id']);
+          break;
+          case 'open':
+            result['envelope_statistics']['events']['open']['count']++;
+            result['envelope_statistics']['events']['open']['leads'].push(value['lead_id']);
+          break;
+          case 'click':
+            result['envelope_statistics']['events']['click']['count']++;
+            result['envelope_statistics']['events']['click']['leads'].push(value['lead_id']);
+          break;
+          case 'deferred':
+            result['envelope_statistics']['events']['deferred']['count']++;
+            result['envelope_statistics']['events']['deferred']['leads'].push(value['lead_id']);
+          break;
+          case 'spam_report':
+            result['envelope_statistics']['events']['spam_report']['count']++;
+            result['envelope_statistics']['events']['spam_report']['leads'].push(value['lead_id']);
+          break;
+          case 'spam':
+            result['envelope_statistics']['events']['spam']['count']++;
+            result['envelope_statistics']['events']['spam']['leads'].push(value['lead_id']);
+          break;
+          case 'unsubscribe':
+            result['envelope_statistics']['events']['unsubscribe']['count']++;
+            result['envelope_statistics']['events']['unsubscribe']['leads'].push(value['lead_id']);
+          break;
+          case 'drop':
+            result['envelope_statistics']['events']['drop']['count']++;
+            result['envelope_statistics']['events']['drop']['leads'].push(value['lead_id']);
+          break;
+          case 'bounce':
+            result['envelope_statistics']['events']['bounce']['count']++;
+            result['envelope_statistics']['events']['bounce']['leads'].push(value['lead_id']);
+          break;
+        }
+      });
+      return result;
+    }
+    }
+  end
 end
--- spec/mailcannon/map_reduce_spec.rb
+++ spec/mailcannon/map_reduce_spec.rb
@@ -9,17 +9,17 @@ describe MailCannon::MapReduce do
     envelope_b.save
     envelope_c.save
     test_hash = [
-      {envelope_id: envelope_a.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
-      {envelope_id: envelope_a.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'open'},
-      {envelope_id: envelope_a.id, email: 'foo3@bar.com', timestamp: 1322000094, unique_arg: 'my unique arg', event: 'bounce'},
-      {envelope_id: envelope_b.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'},
-      {envelope_id: envelope_b.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'click'},
-      {envelope_id: envelope_c.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered'}
+      {envelope_id: envelope_a.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered', lead_id: '1'},
+      {envelope_id: envelope_a.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'open', lead_id: '2'},
+      {envelope_id: envelope_a.id, email: 'foo3@bar.com', timestamp: 1322000094, unique_arg: 'my unique arg', event: 'bounce', lead_id: '3'},
+      {envelope_id: envelope_b.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'delivered', lead_id: '1'},
+      {envelope_id: envelope_b.id, email: 'foo2@bar.com', timestamp: 1322000093, unique_arg: 'my unique arg', event: 'click', lead_id: '2'},
+      {envelope_id: envelope_c.id, email: 'foo1@bar.com', timestamp: 1322000092, unique_arg: 'my unique arg', event: 'click', lead_id: '1'}
     ]
     MailCannon::SendgridEvent.insert_bulk(test_hash)
   end
 
-  describe "#grab_events_for_envelope" do
+  describe ".grab_events_for_envelope" do
     it "inserts the events into the envelope" do
       MailCannon::MapReduce.grab_events_for_envelope(envelope_a.id)
       expect(envelope_a.reload.sendgrid_events.count).to eq(3)
@@ -35,6 +35,25 @@ describe MailCannon::MapReduce do
       expect do
         MailCannon::MapReduce.grab_events_for_envelope(envelope_a.id)
       end.to change {envelope_a.sendgrid_events.pluck(:processed)}.to match_array([false,false,false])
+    end
+  end
+
+  describe ".statistics_for_envelope" do
+    it "returns statistics hash/json" do
+      mapreduce = MailCannon::MapReduce.statistics_for_envelope(envelope_a.id)
+      expected_hash = {       
+        "posted"=>{"count"=>0.0, "leads"=>[]},
+        "processed"=>{"count"=>0.0, "leads"=>[]},
+        "delivered"=>{"count"=>1.0, "leads"=>["1"]},
+        "open"=>{"count"=>1.0, "leads"=>["2"]},
+        "click"=>{"count"=>0.0, "leads"=>[]},
+        "deferred"=>{"count"=>0.0, "leads"=>[]},
+        "spam_report"=>{"count"=>0.0, "leads"=>[]},
+        "spam"=>{"count"=>0.0, "leads"=>[]},
+        "unsubscribe"=>{"count"=>0.0, "leads"=>[]},
+        "drop"=>{"count"=>0.0, "leads"=>[]},
+        "bounce"=>{"count"=>1.0, "leads"=>["3"]}}
+      expect(mapreduce.raw['results'].first['value']['envelope_statistics']['events']).to eq(expected_hash)
     end
   end
 
