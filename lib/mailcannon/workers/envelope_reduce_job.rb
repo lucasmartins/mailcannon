dd8c2a17e0fe858c0e3300598a7ff1bc83b65bf8
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-03-14 10:10:05 -0300

    adding js files, job testing, minor refactoring

--- lib/mailcannon/workers/envelope_reduce_job.rb
+++ lib/mailcannon/workers/envelope_reduce_job.rb
@@ -3,6 +3,7 @@ class MailCannon::EnvelopeReduceJob
   
   def perform(envelope_ids)
     envelope_ids.each do |id|
+      id = id['$oid'] if id['$oid']
       MailCannon::EnvelopeMapReduce.statistics_for_envelope(id)
     end
   end
