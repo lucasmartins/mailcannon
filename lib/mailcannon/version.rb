58c7323016c6391fbfea2764f17a4c059b2444c1
Lucas Martins <lucasmartins@railsnapraia.com>
2014-04-05 09:23:26 -0300

    bumping up version

--- lib/mailcannon/version.rb
+++ lib/mailcannon/version.rb
@@ -2,8 +2,8 @@
 module MailCannon
   module Version
     MAJOR = 0
-    MINOR = 0
-    PATCH = 8
+    MINOR = 1
+    PATCH = 0
     ALPHA = '.pre.1'
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}#{ALPHA}"
   end
