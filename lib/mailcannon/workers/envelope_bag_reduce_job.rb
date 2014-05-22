e69d3818595f6ef12014822d18901e3e3d2af8da
Danilo Delponte <danilo.delponte@resultadosdigitais.com.br>
2014-05-22 10:25:14 -0300

    fix jobs workers queues

--- lib/mailcannon/workers/envelope_bag_reduce_job.rb
+++ lib/mailcannon/workers/envelope_bag_reduce_job.rb
@@ -1,11 +1,13 @@
 class MailCannon::EnvelopeBagReduceJob
   include Sidekiq::Worker
-  
+
+  sidekiq_options :queue => :event_processing
+
   def perform(envelope_bag_ids)
     envelope_bag_ids.each do |id|
       id = id['$oid'] if id['$oid']
       MailCannon::EnvelopeBag.reduce_statistics_for_envelope_bag(id)
     end
   end
-  
+
 end
