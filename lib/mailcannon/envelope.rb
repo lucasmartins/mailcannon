d3446f6a0db03c4011a3b0ba5d8c5ffcaca560fe
Henrich Moraes <henrich.moraes@resultadosdigitais.com.br>
2019-03-19 14:14:18 -0300

    Config optional: true to model work with Rails 5
    Why? In Rails 5 all `belongs_to` associations the `required` option will
    be set to true by default, so we need to set the option `optional` with
    true where the association isn't required

--- lib/mailcannon/envelope.rb
+++ lib/mailcannon/envelope.rb
@@ -4,7 +4,7 @@ class MailCannon::Envelope
   include Mongoid::Timestamps
   include MailCannon::Adapter::SendgridWeb
 
-  belongs_to :envelope_bag, index: true
+  belongs_to :envelope_bag, index: true, optional: true
 
   embeds_one :mail
   embeds_many :stamps
